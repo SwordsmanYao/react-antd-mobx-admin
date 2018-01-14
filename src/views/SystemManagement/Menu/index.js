ac104b383a01d2fe5cc57d5ba0fc634b4bdd9e04
SwordsmanYao <swordsman.yao@foxmail.com>
2018-01-14 19:18:13 +0800

    路由例子

--- src/views/SystemManagement/Menu/index.js
+++ src/views/SystemManagement/Menu/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 
-
+@inject('routing')
 @inject('global')
 @observer
 class Menu extends Component {
@@ -9,9 +9,13 @@ class Menu extends Component {
   render() {
 
     const { global } = this.props;
+    const { location, push, goBack } = this.props.routing;
 
     return (
       <div>
+        <span>Current pathname: {location.pathname}</span>		
+        <button onClick={() => push('/basic')}>Change url</button>		
+        <button onClick={() => goBack()}>Go Back</button>
         <div>{global.number}</div>
         <button onClick={() => global.inc()}>+</button>
       </div>
